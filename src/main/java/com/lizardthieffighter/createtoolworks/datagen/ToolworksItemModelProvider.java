e241854e2313e1dd6550800028915f4ac38ed28a
Morgan Schulz <mschulz0451@aol.com>
2026-09-10 19:45:52 -0500

    Add drill item model datagen

---
+++ src/main/java/com/lizardthieffighter/createtoolworks/datagen/ToolworksItemModelProvider.java
@@ -0,0 +1,20 @@
+package com.lizardthieffighter.createtoolworks.datagen;
+
+import com.lizardthieffighter.createtoolworks.CreateToolworks;
+import com.lizardthieffighter.createtoolworks.registry.ModItems;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+public class ToolworksItemModelProvider extends ItemModelProvider {
+    public ToolworksItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, CreateToolworks.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        // Temporary placeholder: render the drill using Minecraft's stick model.
+        withExistingParent(ModItems.MECHANICAL_DRILL.getId().getPath(), mcLoc("item/stick"));
+    }
+}

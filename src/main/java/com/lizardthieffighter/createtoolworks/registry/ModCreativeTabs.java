7a6517e9c7c2a84869e1e26f4d3456f6a3f6bc69
Morgan Schulz <mschulz0451@aol.com>
2026-09-10 19:45:42 -0500

    Add Create: Toolworks creative tab

---
+++ src/main/java/com/lizardthieffighter/createtoolworks/registry/ModCreativeTabs.java
@@ -0,0 +1,30 @@
+package com.lizardthieffighter.createtoolworks.registry;
+
+import com.lizardthieffighter.createtoolworks.CreateToolworks;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import java.util.function.Supplier;
+
+public final class ModCreativeTabs {
+    private ModCreativeTabs() {}
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(net.minecraft.core.registries.Registries.CREATIVE_MODE_TAB, CreateToolworks.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> TOOLWORKS_TAB = CREATIVE_MODE_TABS.register(
+            "toolworks",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.createtoolworks.toolworks"))
+                    .icon(() -> new ItemStack(ModItems.MECHANICAL_DRILL.get()))
+                    .displayItems((parameters, output) -> output.accept(ModItems.MECHANICAL_DRILL.get()))
+                    .build()
+    );
+
+    public static void register(net.neoforged.bus.api.IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
